002e7013db833b58edc2bd7d63dbed97f7b78b29
djtonzera <ewertong97@gmail.com>
2025-03-14 07:39:51 -0300

    primeiro commit

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "O que significa HTML?",
+        answer: "C. Linguagem de Marcação de Hipertexto",
+        options: [
+            "A. Tipo Hiper Multi Linguagem",
+            "B. Texto Hiper Múltipla Linguagem",
+            "C. Linguagem de Marcação de Hipertexto",
+            "D. Texto Doméstico Multi Linguagem"
+        ]
+    },
+    {
+        numb: 2,
+        question: "O que significa CSS?",
+        answer: "A. Folha de Estilo em Cascata",
+        options: [
+            "A. Folha de Estilo em Cascata",
+            "B. Folha de Estilo Bonita",
+            "C. Folha de Estilo de Computador",
+            "D. Folha de Estilo Codehal"
+        ]
+    },
+    {
+        numb: 3,
+        question: "O que significa PHP?",
+        answer: "A. Pré-processador de Hipertexto",
+        options: [
+            "A. Pré-processador de Hipertexto",
+            "B. Programação de Texto Doméstico",
+            "C. Pré-programação de Hipertexto",
+            "D. Programação Pré-processador de Hipertexto"
+        ]
+    },
+    {
+        numb: 4,
+        question: "O que significa SQL?",
+        answer: "D. Linguagem de Consulta Estruturada",
+        options: [
+            "A. Linguagem de Consulta de Força",
+            "B. Linguagem de Consulta de Folha de Estilo",
+            "C. Linguagem de Perguntas Científicas",
+            "D. Linguagem de Consulta Estruturada"
+        ]
+    },
+    {
+        numb: 5,
+        question: "O que significa XML?",
+        answer: "D. Linguagem de Marcação Extensível",
+        options: [
+            "A. Múltipla Linguagem Excelente",
+            "B. Explorar Múltipla Linguagem",
+            "C. Linguagem de Marcação Extra",
+            "D. Linguagem de Marcação Extensível"
+        ]
+    }
+];
